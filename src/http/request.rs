1890b085c67aaaa0c7bd38504865e75bd8ab5a93
Aoi <ht.so.ik0102@gmail.com>
2021-09-26 12:24:52 +0700

    change

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,28 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    // GET /serach?name-abc&sort=1 HTTP/1.1
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+

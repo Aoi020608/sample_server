ebbdf6806df46e490ef09c49f7b8a8c65cf8fab2
Aoi <ht.so.ik0102@gmail.com>
2021-09-26 11:51:37 +0700

    modify run function

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -17,10 +18,15 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok(_) => {
-                    let a = 5;
-                    println!("OK")
-                },
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    };
+                }
                 Err(e) => println!("Faild t oestablish a connection: {}", e),
             }
         }

085b8148f32792621d60cc988429e77379841f05
Aoi <ht.so.ik0102@gmail.com>
2021-09-26 11:23:16 +0700

    modify run function

--- src/server.rs
+++ src/server.rs
@@ -14,5 +14,15 @@ impl Server {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok(_) => {
+                    let a = 5;
+                    println!("OK")
+                },
+                Err(e) => println!("Faild t oestablish a connection: {}", e),
+            }
+        }
     }
 }

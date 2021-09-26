456a73018dc98190f0cf7dc6323ef8ff18920952
Aoi <ht.so.ik0102@gmail.com>
2021-09-26 15:35:55 +0700

    change try_from

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,9 @@
 use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -9,13 +12,56 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     // GET /serach?name-abc&sort=1 HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        // match str::from_utf8(buf) {
+        //     Ok(request) => {}
+        //     Err(_) => return Err(ParseError::InvalidEncoding),
+        // }
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+        //     Ok(request) => {}
+        //     Err(e) => return Err(e),
+        // }
+
+        let request = str::from_utf8(buf)?;
+
+        // match get_next_word(request) {
+        //     Some((method, request)) => {},
+        //     None => return Err(ParseError::InvalidRequest),
+        // }
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
         unimplemented!()
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    // let mut iter = request.chars();
+    // loop {
+    //     let item = iter.next();
+    //     match item {
+    //         Some(c) => {},
+    //         None => break,
+    //     }
+    // }
+
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+
+    None
 }
 
 pub enum ParseError {
@@ -25,4 +71,33 @@ pub enum ParseError {
     InvalidMethod,
 }
 
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
 
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}

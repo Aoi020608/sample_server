09cd6c5f77051b65e49168796b6da3ef75f8cced
Aoi <ht.so.ik0102@gmail.com>
2021-09-26 22:55:34 +0700

    change response

--- src/server.rs
+++ src/server.rs
@@ -26,17 +26,23 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
-                                    let response = Response::new(
+                                    Response::new(
                                         StatusCode::Ok,
                                         Some("<h1>It workds</h1>".to_string()),
                                     );
-                                    write!(stream, "{}", response);
                                 }
-                                Err(e) => println!("Failed to parse a request: {}", e),
+                                Err(e) => {
+                                    println!("Failed to parse a request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None);
+                                },
                             };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     };

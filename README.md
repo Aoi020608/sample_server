48fa32d27fcf2d8ae663840b2405f1bae1306bfd
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-13 22:07:17 +0900

    feat: async await2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sample-server
+
+## REFERENCES
+- https://www.youtube.com/watch?v=ThjvMReOXYM
--- examples/patience.rs
+++ examples/patience.rs
@@ -5,6 +5,7 @@ use std::{future::Future, io::BufRead};
 fn main() {
     println!("Hello, world!");
 
+    // one future
     let read_from_terminal = std::thread::spawn(move || {
         let mut x = std::io::Stdin::lock(&std::io::stdin());
         for line in x.lines() {
@@ -12,14 +13,18 @@ fn main() {
         }
     });
 
-    let read_from_network = std::thread::spawn(mvoe || {
-        let mut x = std::net::TcpStream::bind("0.0.0.:8080").unwrap();
+    // two future
+    let read_from_network = std::thread::spawn(move || {
+        let mut x = std::net::TcpListener::bind("0.0.0.0:8080").unwrap();
         while let Ok(stream) = x.accept() {
             let handle = std::thread::spawn(move || {
                 // handle_connection(stream);
             });
         }
     });
+
+    let network = read_from_network();
+    let termianl = read_from_terminal();
 
     let x = foo2();
 }
@@ -29,36 +34,24 @@ async fn foo1() -> usize {
     0
 }
 
+async fn read_to_string(s: &str) -> &str {""}
+
+fn expensive_function(x: &str) {}
+
 // future trait means signifies a value that's not ready yet
 // but it will eventually be a usize
 fn foo2() -> impl Future<Output = usize> {
     async {
-        
-        // let x = read_to_string("file").await;
-        //
-        // let fut = read_to_string("file");
-        // let x = loop {
-        //      if let Some(result) = fut.try_check_completed() {
-        //          break result;
-        //      } else {
-        //          fut.try_make_pregress();
-        //          yield;
-        //      }
-        // };
-
-
-        // First time:
         println!("foo1");
-        read_to_string("file1").await; // Wait here
-        // First time:
-        println!("foo1");
-        read_to_string("file2").await; // Wait here
-        // First time:
-        println!("foo1");
-        read_to_string("file3").await; // Wait here
-        // First time:
-        println!("foo1");
-        read_to_string("file4").await; // Wait here
+        read_to_string("file1").await;
+        println!("foo2");
+        read_to_string("file2").await;
+        println!("foo3");
+        let x = read_to_string("file3").await;
+        println!("file1");
+        expensive_function(x);
+        println!("file1");
+        println!("file1");
         0
     }
 }

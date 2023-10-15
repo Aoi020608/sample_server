9b8d5da5e5f19a6de89304520cd520cec550a1fb
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-15 11:49:38 +0900

    feat: tokio select

--- examples/patience.rs
+++ examples/patience.rs
@@ -11,6 +11,26 @@ fn main() {
     runtime.block_on(async {
         println!("Hello, world!");
 
+        let mut network = read_from_network();
+        let mut termial = read_from_termianl();
+        let mut foo = foo2();
+
+        let mut f1 = tokio::fs::File::open("foo");
+        let mut f2 = tokio::fs::File::open("bar");
+        let copy = tokio::io::copy(&mut f1, &mut f2);
+
+        select! {
+            stream <- (&mut network).await => {
+                // do something
+            }
+            line <- (&mut terminal).await => {
+                // do something with line
+                break;
+            }
+            foo <- (&mut foo).await => {}
+            
+        }
+
         // await means don't run the following lists of of instructions until foo1 actually resolved
         // into its output type
         let x = foo1().await;

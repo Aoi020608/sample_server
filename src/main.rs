73bfde995bdd9bc18124b0f9e7438491aadef83a
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-29 07:15:33 +0900

    fix: change logic

--- src/main.rs
+++ src/main.rs
@@ -24,18 +24,21 @@ fn main() -> io::Result<()> {
             Ok(_) => {
                 let request = String::from_utf8_lossy(&buf);
 
-                println!("{:?}", request);
-
                 match extract_path(&request) {
                     Some(path) => {
-                        let paths: Vec<&str> = path.split('/').collect();
+                        let children: Vec<&str> = path.split('/').collect();
 
-                        if paths[1] == "echo" {
-                            let res = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n{}\r\n\r\n", paths[2].len(), paths[2]);
-                            socket.write(res.as_bytes())?
+                        let res = if path == "/" {
+                            "HTTP/1.1 200 OK\r\n\r\n".to_string()
                         } else {
-                            socket.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?
-                        }
+                            if children[1] == "echo" {
+                                format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n{}\r\n\r\n", children[2].len(), children[2])
+                            } else {
+                                "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                            }
+                        };
+
+                        socket.write(res.as_bytes())?
                     }
                     None => socket.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?,
                 }

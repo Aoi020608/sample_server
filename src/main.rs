b2ece0fb0c1333c8ad2249e01ec2f77f83a79c02
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-28 16:40:55 +0900

    feat: implement main function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,53 @@
 #![allow(dead_code)]
 
+use std::{
+    io::{self, Read, Write},
+    net::TcpListener,
+};
+
 use server::Server;
-use std::env;
 use website_handler::WebsiteHandler;
 
 mod http;
 mod server;
 mod website_handler;
 
-fn main() {
-    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
-    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
-    println!("public path: {}", public_path);
-    let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler::new(public_path));
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    loop {
+        let (mut socket, _) = listener.accept().unwrap();
+
+        let mut buf = [0; 1024];
+
+        match socket.read(&mut buf) {
+            Ok(_) => {
+                let request = String::from_utf8_lossy(&buf);
+
+                match extract_path(&request) {
+                    Some(path) => {
+                        if path == "/" {
+                            socket.write(b"HTTP/1.1 200 OK\r\n\r\n")?
+                        } else {
+                            socket.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?
+                        }
+                    }
+                    None => socket.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?,
+                }
+            }
+            Err(_e) => socket.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?,
+        };
+    }
+}
+
+fn extract_path(req: &str) -> Option<&str> {
+    for (idx, line) in req.lines().enumerate() {
+        if idx == 0 {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+
+            return parts.get(1).copied();
+        }
+    }
+
+    None
 }

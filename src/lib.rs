b2e469bb94376447136f2c443823be869ed160f8
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-20 20:17:51 +0900

    feat: add movie review

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,39 @@
+use instruction::MovieInstruction;
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
+pub mod instruction;
+
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
-    _instruction_data: &[u8],
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!("Hello World!");
-    Ok(())
+    let instruction = MovieInstruction::unpack(instruction_data)?;
+
+    match instruction {
+        MovieInstruction::AddMovieReview {
+            title,
+            rating,
+            description,
+        } => add_movie_review(program_id, accounts, title, rating, description),
+    }
 }
 
+pub fn add_movie_review(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    title: String,
+    rating: u8,
+    description: String,
+) -> ProgramResult {
+    msg!("Adding movie review...");
+    msg!("Title: {}", title);
+    msg!("Rating: {}", rating);
+    msg!("Description: {}", description);
+
+    Ok(())
+}

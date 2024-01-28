42cb308fe87881329db013e79ce400b9c3b946cd
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-28 10:34:01 +0900

    feat: call program with rust client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,108 @@
-fn main() {
-    println!("Hello, world!");
+use std::str::FromStr;
+
+use anchor_client::{anchor_lang::system_program, solana_sdk::signature::Keypair, Client, Cluster};
+use clap::{Parser, Subcommand};
+use dotenv::dotenv;
+use hahatoco::accounts as hahatoco_accounts;
+use hahatoco::instruction as hahatoco_instruction;
+use solana_sdk::signature::read_keypair_file;
+use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey, signer::Signer};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Initialize { program_id: String },
+
+    Increment { program_id: String },
+}
+pub fn main() {
+    dotenv().ok();
+    let cli = Cli::parse();
+    let payer = read_keypair_file(&*shellexpand::tilde("~/.config/solana/id.json"))
+        .expect("requires a keypair file");
+
+    let client = Client::new_with_options(Cluster::Localnet, &payer, CommitmentConfig::confirmed());
+
+    let counter = initialize_keypair();
+
+    match &cli.command {
+        // Initialize
+        Commands::Initialize { program_id } => {
+            let program_id = Pubkey::from_str(program_id).expect("parse program_id to Pubkey");
+            let program = client.program(program_id).expect("");
+
+            let authority = program.payer();
+
+            let sig = program
+                .request()
+                .signer(&counter)
+                .payer(&payer)
+                .accounts(hahatoco_accounts::Initialize {
+                    user: authority,
+                    counter: counter.pubkey(),
+                    system_program: system_program::ID,
+                })
+                .args(hahatoco_instruction::Initialize { authority })
+                .send();
+
+            match sig {
+                Ok(transaction_sig) => {
+                    println!(
+                        "Transaction https://explorer.solana.com/tx/{}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899",
+                        transaction_sig
+                    );
+                }
+                Err(e) => println!("Error: {}", e),
+            }
+        }
+
+        Commands::Increment { program_id } => {
+            let program_id = Pubkey::from_str(program_id).expect("parse program_id to Pubkey");
+            let program = client.program(program_id).expect("");
+
+            let authority = program.payer();
+
+            let sig = program
+                .request()
+                .accounts(hahatoco_accounts::Increment {
+                    counter: counter.pubkey(),
+                    authority,
+                })
+                .args(hahatoco_instruction::Increment {})
+                .send();
+
+            match sig {
+                Ok(transaction_sig) => {
+                    println!(
+                        "Transaction https://explorer.solana.com/tx/{}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899",
+                        transaction_sig
+                    );
+                }
+                Err(e) => println!("Error: {}", e),
+            }
+        }
+    }
+}
+
+pub fn initialize_keypair() -> Keypair {
+    match std::env::var("PRIVATE_KEY") {
+        Ok(private_key) => {
+            println!("Found a keypair");
+            Keypair::from_base58_string(&private_key)
+        }
+        Err(_) => {
+            println!("Generating new keypair...");
+            let signer = Keypair::new();
+            std::fs::write(".env", format!("PRIVATE_KEY={}", signer.to_base58_string()))
+                .expect("write secret key");
+
+            signer
+        }
+    }
 }
--- programs/hahatoco/src/lib.rs
+++ programs/hahatoco/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("95N6ocYECUTFYczRpqCXznCii1iHAF8pAYbQucuxoYTN");
+declare_id!("CiQgCHtk9t7V5C7m1UMKCD7aoz7Egzida1BhVQQ1UnQ4");
 
 #[program]
 pub mod hahatoco {
@@ -8,12 +8,13 @@ pub mod hahatoco {
 
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, authority: Pubkey) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
 
         msg!("Previous Count: {}", counter.count);
 
-        counter.count = counter.count.checked_add(1).unwrap_or(0);
+        counter.authority = authority;
+        counter.count = 0;
 
         msg!("Counter Incremented");
         msg!("Counter count: {}", counter.count);
@@ -21,12 +22,12 @@ pub mod hahatoco {
         Ok(())
     }
 
-    pub fn increment(ctx: Context<Update>) -> Result<()> {
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
 
         msg!("Previous Count: {}", counter.count);
 
-        counter.count = counter.count.checked_add(1).unwrap_or(0);
+        counter.count += 1;
 
         msg!("Counter Incremented");
         msg!("Current count: {}", counter.count);
@@ -37,24 +38,25 @@ pub mod hahatoco {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 40)]
+    pub counter: Account<'info, Counter>,
+
     #[account(mut)]
     pub user: Signer<'info>,
-
-    #[account(init, payer = user, space = 8 + 8)]
-    pub counter: Account<'info, Counter>,
 
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
     pub counter: Account<'info, Counter>,
 
-    pub user: Signer<'info>,
+    pub authority: Signer<'info>,
 }
 
 #[account]
 pub struct Counter {
+    pub authority: Pubkey,
     pub count: u64,
 }
